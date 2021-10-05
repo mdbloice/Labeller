7e11e9ae832fad633aa1267f49f323b50f415d8e
Marcus Bloice <marcus.bloice@medunigraz.at>
2021-10-05 09:33:29 +0200

    Changes for version 0.1.7

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('PyPI.md') as f:
 
 setup(name='labeller',
         packages=['labeller'],
-        version='0.1.6',
+        version='0.1.7',
         description='Create image labelling software for machine learning applications.',
         long_description=long_description,
         long_description_content_type='text/markdown',
